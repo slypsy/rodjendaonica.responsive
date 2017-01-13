ae2d7365933b8d20e17546ab36aefee357a96cca
slypsy <tamarakovacev13@gmail.com>
2017-01-13 15:06:58 +0100

    first commit

---
+++ config.rb
@@ -0,0 +1,15 @@
+require "susy"
+require "braekpoint"
+
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "sass"
+css_dir = "dist/css"
+images_dir = "dist/img"
+fonts_dir = "fonts"
+javascripts_dir = "dist/js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var htmlImport = require('gulp-html-import');
+//var clean = require('gulp-clean');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths: [
+                'node_modules/susy/sass',
+                'node_modules/breakpoint-sass/stylesheets'
+            ]
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
+
+// gulp.task('clean-css', function () {
+//     return gulp.src(['./css','./dist/css'], {read: false}).pipe(clean());
+// });
+
+gulp.task('html-import', function () {
+    return gulp.src('./src/*.html')
+        .pipe(htmlImport('./src/components/'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html-import-thing', ['html-import'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+
+gulp.task('default', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    gulp.watch('./sass/**/*.scss', [/*'clean-css',*/'sass']);
+    gulp.watch('./src/**/*.html', ['html-import-thing']);
+});
+
